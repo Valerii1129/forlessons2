e6758df4325acd2b4c6f702b9106e1a8f443b822
Valerii1129 <yanovskiyvalera1@gmail.com>
2023-07-02 07:48:46 +0100

    lessons 3, 3rd commit

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,10 +1,14 @@
 package lesson3;
 
 class Main {
+    public void test(Animal animal){
+        animal.say();
+    }
     public static void main(String[] args) {
         Object[] obj = new Object[2];
         Cat cat = new Cat("Vasya", 3);
         Dog dog = new Dog("Sharik", 2);
+        Main main = new Main();
         obj[0] = cat;
         try {
             obj[1] = dog;
@@ -13,7 +17,7 @@ class Main {
         } catch (ClassCastException e) {
             System.out.println("Incorrect data type\n");
         }
-        cat.say();//МЯУ
-        dog.say();//ГАВ
+        main.test(cat);
+        main.test(dog);
     }
 }
